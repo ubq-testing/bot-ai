20537c69407b2b4f8a0e5ff24be34e366c01d7fe
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2023-10-21 18:22:15 +0100

    feat: count input
    added count input

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 "use client";
 
 import { useChat } from "ai/react";
+import { useState } from "react";
 
 export default function Dual() {
   const { messages, input, handleInputChange, handleSubmit, data } = useChat();
+  const { count, setCount } = useState(0);
+
   return (
     <div className="flex">
+      <div>
+        <h1>Count: {count}</h1>
+        <input value={count} onChange={(e) => setCount(e.target.value)} />
+      </div>
       <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
         {messages.length > 0
           ? messages.map((m) => (
