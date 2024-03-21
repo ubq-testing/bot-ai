b320eb1725ab158e64fec137f841323032ca9dfb
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-03-21 11:07:51 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Dual() {
               </div>
             ))
           : null}//
-
+//
         <form onSubmit={handleSubmit}>
           <input
             className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
