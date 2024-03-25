a2ee865dfe3d63d7be3739008d64ca30f51dc8e5
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-03-25 15:55:01 +0000

    missed one

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,7 +3,7 @@ import OpenAI from "openai";
 import { OpenAIStream, StreamingTextResponse } from "ai";
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API || "",
+  apiKey: process.env.OPENAI_API,
 });
 
 export const runtime = "edge";
