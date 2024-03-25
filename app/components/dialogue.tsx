5e89c4ccd6d2deba3bd08a4fe90fefe9e0d76f89
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-03-25 13:02:58 +0000

    remove all empty

--- app/components/dialogue.tsx
+++ app/components/dialogue.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function DialogueBox({ title, content }) {
   return (
-    <div className="">
+    <div className= >
       <h2>{title}</h2>
       <p>{content}</p>
     </div>
