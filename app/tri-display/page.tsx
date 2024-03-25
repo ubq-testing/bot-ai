58b7eb9e3c7e96a0f500f4436dcf977adc147d2a
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-03-25 14:55:34 +0000

    better bash string handling

--- app/tri-display/page.tsx
+++ app/tri-display/page.tsx
@@ -2,12 +2,10 @@
 
 import { useChat } from "ai/react";
 
-const shhhheeesh = "";
-
 export default function Dual() {
   const { messages, input, handleInputChange, handleSubmit, data } = useChat();
   return (
-    <div className="flex">
+    <div className="">
       <div className="flex flex-col w-full max-w-md py-24 mx-auto stretch">
         {messages.length > 0
           ? messages.map((m) => (
