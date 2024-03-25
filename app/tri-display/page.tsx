83c34c7062e93b9108c51872480604cf6b7ade12
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-03-25 15:38:06 +0000

    nada

--- app/tri-display/page.tsx
+++ app/tri-display/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useChat } from "ai/react";
-//
+const l = "";
 export default function Dual() {
   const { messages, input, handleInputChange, handleSubmit, data } = useChat();
   return (
