9ae8d5350dfddc85128c8faf18af20acc5f95df5
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-03-25 13:14:44 +0000

    add empties back

--- app/components/dialogue.tsx
+++ app/components/dialogue.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function DialogueBox({ title, content }) {
   return (
     <div>
-      <h2>{title}</h2>
+      <h2>{""}</h2>
       <p>{content}</p>
     </div>
   );
