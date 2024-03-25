d909a38d68d2d311988ec6fc34795bf055e3a367
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-03-25 15:50:55 +0000

    singular

--- app/tri-display/layout.tsx
+++ app/tri-display/layout.tsx
@@ -1,14 +1,10 @@
-export const metadata = {
-  description: "",
-};
-
 export default function DualLayout(props: {
   children: React.ReactNode;
   data: React.ReactNode;
   docs: React.ReactNode;
 }) {
   return (
-    <div className="grid grid-cols-3 shrink">
+    <div className="">
       {props.children}
       {props.docs}
     </div>
