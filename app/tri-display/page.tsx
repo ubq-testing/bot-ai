749d461478a0022ac58fd27bb0e65c4f9d664a77
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2024-03-25 14:47:47 +0000

    add other instance in problem files

--- app/tri-display/page.tsx
+++ app/tri-display/page.tsx
@@ -1,7 +1,9 @@
 "use client";
 
 import { useChat } from "ai/react";
-//
+
+const shhhheeesh = "";
+
 export default function Dual() {
   const { messages, input, handleInputChange, handleSubmit, data } = useChat();
   return (
@@ -20,7 +22,7 @@ export default function Dual() {
           <input
             className="fixed bottom-0 w-full max-w-md p-2 mb-8 border border-gray-300 rounded shadow-xl"
             value={input}
-            placeholder=""
+            placeholder="Say something..."
             onChange={handleInputChange}
           />
         </form>
